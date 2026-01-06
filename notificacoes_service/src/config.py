0c5ec5313535480dbd00b30b8d881146d8d18436
JoaoMNZ <joao.menezes21@Outlook.com>
2026-01-06 10:18:46 -0300

    Sobe aplicações clientes para Dockerhub e utiliza elas no docker-compose.yml.

--- notificacoes_service/src/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    RABBITMQ_HOST = os.getenv("RABBITMQ_HOST")
-    RABBITMQ_PORT = int(os.getenv("RABBITMQ_PORT"))
-    RABBITMQ_USER = os.getenv("RABBITMQ_USER")
-    RABBITMQ_PASSWORD = os.getenv("RABBITMQ_PASSWORD")
-
-    NOTIFICATION_EXCHANGE = "notifications"
